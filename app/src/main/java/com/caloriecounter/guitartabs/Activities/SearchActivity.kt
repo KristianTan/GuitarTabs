990dca30980c5d2ac9b2aab1c10a51dbcaef5530
kristian <kristian.tan@ntlworld.com>
2019-05-18 16:44:40 +0100

    Begun search feature

--- app/src/main/java/com/caloriecounter/guitartabs/Activities/SearchActivity.kt
+++ app/src/main/java/com/caloriecounter/guitartabs/Activities/SearchActivity.kt
@@ -1,22 +1,20 @@
 package com.caloriecounter.guitartabs.Activities
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
 import android.view.Window
 import android.view.WindowManager
 import android.widget.SearchView
-import android.widget.TextView
 import com.caloriecounter.guitartabs.R
 import com.caloriecounter.guitartabs.Requests.AsyncAPICall
 
-class MainActivity : AppCompatActivity() {
-
+class SearchActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
-            R.id.navigation_search -> {
-                val intent = Intent(this, SearchActivity::class.java)
+            R.id.navigation_saved -> {
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
 
                 return@OnNavigationItemSelectedListener true
@@ -26,20 +24,32 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
         requestWindowFeature(Window.FEATURE_NO_TITLE)
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        setContentView(R.layout.activity_main)
-
-        val task = AsyncAPICall()
-        task.execute("wonderwall")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
 
         val bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_navigation)
         bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        val search : SearchView = this.findViewById(R.id.searchBar)
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+                val task = AsyncAPICall()
+                task.execute(query)
+                return false
+            }
+
+        })
+
     }
 }

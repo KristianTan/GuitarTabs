7f0c80f5f94972c36568486a931b8686f1303c19
kristian <kristian.tan@ntlworld.com>
2019-05-18 15:35:48 +0100

    Added bottom navigation bar

--- app/src/main/java/com/caloriecounter/guitartabs/Activities/MainActivity.kt
+++ app/src/main/java/com/caloriecounter/guitartabs/Activities/MainActivity.kt
@@ -2,12 +2,29 @@ package com.caloriecounter.guitartabs.Activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.view.Window
 import android.view.WindowManager
+import android.widget.SearchView
+import android.widget.TextView
 import com.caloriecounter.guitartabs.R
 import com.caloriecounter.guitartabs.Requests.AsyncAPICall
 
 class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_saved -> {
+                println("saved")
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_search -> {
+                println("search")
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,10 +35,12 @@ class MainActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        val task = AsyncAPICall()
-        task.execute()
-
+//        val task = AsyncAPICall()
+//        task.execute()
 
         setContentView(R.layout.activity_main)
+
+        val bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
